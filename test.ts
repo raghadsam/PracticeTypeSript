77d117d7213557f1d65f1ed659e0e24274412006
Raghad Al Samrout <raghad77samrout@gmail.com>
2021-09-26 08:29:56 +0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,3 +3,6 @@ import { Test } from "./practice";
 let x = new Test();
 x.name = "test";
 console.log(x);
+
+//If I run "tsc test.ts" in the console, two js files will be generated (test.js and practice.js)
+//because in test.ts, we imported a class in practice.ts
