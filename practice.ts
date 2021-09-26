4eb0e8c9a10c4fbd4440353bc4aed8bda5b2a630
raghadsam <raghad66samrout@gmail.com>
2021-09-26 08:07:46 +0300

    interfacing

--- practice.ts
+++ practice.ts
@@ -11,6 +11,9 @@ class Person {
     console.log(this.name);
   }
 }
+
+//Student: Derived Class
+//Person: Base Class
 class Student extends Person {
   academicyear: number;
 }
@@ -21,6 +24,45 @@ person.name = "Jack";
 let student = new Student();
 student.name = "Raghad";
 student.academicyear = 2021;
-
 student.SayMyName();
+
 console.log(student);
+
+//interface
+
+interface Notifier {
+  SendNotification(message, address);
+}
+
+class Pollution {
+  GetPollution(n: Notifier) {
+    ///
+    ///
+    ///
+    if (pollution > 30) {
+      //send SMS
+      //send Email
+      n.SendNotification("", "");
+    }
+  }
+}
+//Client
+
+class SMSNotifier implements Notifier {
+  SendNotification(message, address) {
+    /// send sms
+  }
+}
+
+class EmailNotifier implements Notifier {
+  SendNotification(message, email) {
+    /// send email
+  }
+}
+let sms = new SMSNotifier();
+let m = new Pollution();
+m.GetPollution(sms);
+
+let email = new EmailNotifier();
+let r = new Pollution();
+r.GetPollution(email);
